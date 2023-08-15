a493b93891dc36acc73511d14c0f0a59ead0b4f6
Ahmad Fadhil <herrahmadfadhil@gmail.com>
2023-08-15 14:51:45 +0200

    gateway added 2

---
+++ gateway-app/src/test/java/htwb/ai/gatewayapp/GatewayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package htwb.ai.gatewayapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
